3a2da8b8de17b60dc6ee7984207a516f7cf38fb6
WataruTomita <tomi.symphonia@gmail.com>
2021-01-05 11:04:59 +0000

    [fix]3-8 system_compreated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  # ルートにtopを
+  # top page
   root to: 'books#top'
+  # post 'creater_books' => 'books#create'
 end
